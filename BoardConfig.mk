df77673a48f55e68304bd7c254488e12ce0d2671
JarlPenguin <jarlpenguin@outlook.com>
2019-06-21 14:40:56 +0300

    cactus: set minimum firmware requirements
    Signed-off-by: JarlPenguin <jarlpenguin@outlook.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,6 +1,11 @@
 # Inherit from xiaomi mt6765-common
 -include device/xiaomi/mt6765-common/BoardConfigCommon.mk
 
+DEVICE_PATH := device/xiaomi/cactus
+
+# Board
+TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
+
 # Kernel
 TARGET_KERNEL_CONFIG := cactus_defconfig
 
