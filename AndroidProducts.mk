14c8e5b8c81cfd1d972062d87c7c692dd94f29c3
Jarl-Penguin <jarlpenguin@outlook.com>
2019-06-25 22:22:52 +0300

    cactus: CarbonROM bringup
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,2 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/carbon_cactus.mk
--- carbon_cactus.mk
+++ carbon_cactus.mk
@@ -1,12 +1,13 @@
 # Inherit from device
 $(call inherit-product, device/xiaomi/cactus/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common CarbonROM stuff.
+$(call inherit-product, vendor/carbon/config/common.mk)
+$(call inherit-product, vendor/carbon/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := cactus
-PRODUCT_NAME := lineage_cactus
+PRODUCT_NAME := carbon_cactus
 PRODUCT_BRAND := xiaomi
 PRODUCT_MODEL := Redmi 6A
 PRODUCT_MANUFACTURER := Xiaomi
