5a11dfbc900ce06ee2f67f0634493a6bb7130f28
Jarl-Penguin <jarlpenguin@outlook.com>
2019-08-07 16:09:49 +0300

    cactus: switch to MIUI 9.8.1 fingerprint
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- lineage_cactus.mk
+++ lineage_cactus.mk
@@ -13,8 +13,8 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 # Override fingerprint from stock
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="cactus-user 8.1.0 O11019 V10.2.8.0.OCBMIXM release-keys" \
+    PRIVATE_BUILD_DESC="cactus-user 9 PPR1.180610.011 9.8.1 release-keys" \
     PRODUCT_NAME="cactus" \
     TARGET_DEVICE="cactus"
 
-BUILD_FINGERPRINT := xiaomi/cactus/cactus:8.1.0/O11019/V10.2.8.0.OCBMIXM:user/release-keys
+BUILD_FINGERPRINT := xiaomi/cactus/cactus:9/PPR1.180610.011/9.8.1:user/release-keys
