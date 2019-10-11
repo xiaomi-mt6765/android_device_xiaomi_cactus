ec6d2cffcee3d7ff12aa6e6bdbbec5391e142eae
Jarl-Penguin <jarlpenguin@outlook.com>
2019-10-11 20:51:59 +0300

    cactus: enforce MIUI 10.4.4 vendor
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- lineage_cactus.mk
+++ lineage_cactus.mk
@@ -13,8 +13,8 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 # Override fingerprint from stock
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="cactus-user 9 PPR1.180610.011 9.8.1 release-keys" \
+    PRIVATE_BUILD_DESC="cactus-user 9 PPR1.180610.011 V10.4.4.0.PCBCNXM release-keys" \
     PRODUCT_NAME="cactus" \
     TARGET_DEVICE="cactus"
 
-BUILD_FINGERPRINT := xiaomi/cactus/cactus:9/PPR1.180610.011/9.8.1:user/release-keys
+BUILD_FINGERPRINT := xiaomi/cactus/cactus:9/PPR1.180610.011/V10.4.4.0.PCBCNXM:user/release-keys
