70f3f5bb07bba25b4a0ec06801cef46ec8da30b5
Jarl-Penguin <jarlpenguin@outlook.com>
2019-10-11 20:35:41 +0300

    cactus: build kernel from source again
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,8 +7,7 @@ DEVICE_PATH := device/xiaomi/cactus
 TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 
 # Kernel
-# TARGET_KERNEL_CONFIG := cactus_defconfig
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/zImage-dtb
+TARGET_KERNEL_CONFIG := cactus_defconfig
 
 # OTA
 TARGET_OTA_ASSERT_DEVICE := cactus
